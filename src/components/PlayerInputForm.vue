173d79d299a20f3073d5a7f40a0f0aa4064559a6
leonkorth <leonkorth@web.de>
2022-08-18 17:37:04 +0200

    player input form styling

--- src/components/PlayerInputForm.vue
+++ src/components/PlayerInputForm.vue
@@ -1,25 +1,31 @@
 <template>
-  <div class="container">
+  <div class="container text-center">
     <form>
-      <div class="row">
-        <div class="col-4">
-          <div class="form-floating">
+      <div class="row justify-content-start container">
+        <div class="col-lg-4 my-1">
+          <div class="form-floating" >
             <input type="text" class="form-control" id="floatingInputGrid" placeholder="Neuen Spieler hinzufügen...">
-            <label for="floatingInputGrid">Neuen Spieler hinzufügen</label>
+            <label  for="floatingInputGrid">Neuen Spieler hinzufügen</label>
           </div>
         </div>
-        <div class="col-4">
+        <div class="col-lg-4 my-1">
           <div class="form-floating">
             <select class="form-select" id="floatingSelectGrid">
-              <option value="" selected>Spieler auswählen</option>
+              <option class="text-center"  style="text-align-last: center" value="" selected disabled>Spieler auswählen</option>
               <option v-for="player in this.players" :key="player.id" :value="player.id">
                 {{ player.name }}
               </option>
             </select>
           </div>
         </div>
-        <div class="col-2">
-        <button type="submit" class="btn btn-primary">Hinzufügen</button>
+
+      </div>
+      <div class="row justify-content-start container">
+        <div class="col-lg-4 my-1">
+          <button type="submit" class="btn btn-primary">Hinzufügen</button>
+        </div>
+        <div class="col-lg-4 my-1">
+          <button type="submit" class="btn btn-primary">Hinzufügen</button>
         </div>
       </div>
     </form>
